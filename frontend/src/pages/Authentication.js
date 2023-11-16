18830293bde99ff814283e475e30bcafc83328ba
Wasim Hyder <wasim@creativefaze.com>
2023-11-16 15:39:29 +0500

    used auth token

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -37,7 +37,9 @@ export async function action({ request }) {
     throw json({ message: 'Could not authenticate user' }, { status: 500 });
   }
 
-  //soon: manage token
+  const resData = await response.json();
+  const token = resData.token;
+  localStorage.setItem('token', token);
 
   return redirect('/');
 }
