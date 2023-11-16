fb567e16fc7b2aa330d1c03c84d60558cf9da8f7
Wasim Hyder <wasim@creativefaze.com>
2023-11-16 16:30:51 +0500

    Added token expiration and autologout

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,8 +1,26 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react'
+import { Outlet, useLoaderData, useSubmit } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
+import { getTokenDuration } from '../util/auth';
 
 function RootLayout() {
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+    if (token === 'EXPIRED') {
+      submit(null, { action: '/logout', method: "POST" })
+      return;
+    }
+    const tokenDruation = getTokenDuration();
+    setTimeout(() => {
+      submit(null, { action: '/logout', method: "POST" })
+    }, tokenDruation)
+  }, [token, submit]);
   // const navigation = useNavigation();
 
   return (
